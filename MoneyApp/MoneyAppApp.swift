78d4cdc1e9afddfa28d40cbbb7637f7680de4666
Juan Muller <juanfra500@gmail.com>
2025-08-02 22:38:06 -0700

    Initial Commit

---
+++ MoneyApp/MoneyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyAppApp.swift
+//  MoneyApp
+//
+//  Created by Juan Muller on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct MoneyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
